91d094d32fef306a79b6cb2e4badb96a84ec6e30
Jeremy Wadhams <jwadhams1@yahoo.com>
2014-08-01 21:26:13 -0500

    What I don't know about Python could fill several very large books.

--- raspberrypi/one_note.py
+++ raspberrypi/one_note.py
@@ -12,23 +12,24 @@ pygame.mixer.init()
 
 piano_notes = {}
 
-for note in glob("../piano-notes/*.wav")
-	note_name = re.search("([a-z0-9])\.wav$", note).group(0)
-	piano_notes[note_name] = pygame.mixer.Sound(note)
+for note in glob("../piano-notes/*.wav"):
+    note_name = re.search("([a-z0-9]+)\.wav$", note).group(1)
+    piano_notes[note_name] = pygame.mixer.Sound(note)
 
+print piano_notes
 
 skip_until = 0
 
 while True:
     line = ""
     line = ser.readline()
-	note = re.sub("\s+","", line)
+    note = re.sub("\s+","", line)
     if(note not in piano_notes):
-        print "Didn't recognize", note
-		continue
+        print "Didn't recognize ##" + note + "##"
+        continue
     if(skip_until > time.time()):
         print "Waiting for" , skip_until , "is now" , time.time()
-		continue
-	print "Playing", note
+        continue
+    print "Playing", note
     piano_notes[note].play()
     skip_until = time.time() + 2
