292be752ed33b24a7c3682b4da54a0f6344a804d
Jeremy Wadhams <jwadhams1@yahoo.com>
2014-10-06 22:07:15 -0500

    Reworking to allow multiple sensors on the bus.

--- raspberrypi/i2c_notes.py
+++ raspberrypi/i2c_notes.py
@@ -5,8 +5,6 @@ import time
 from glob import glob
 
 bus = smbus.SMBus(1)
-address = 0x08;
-
 
 pygame.mixer.pre_init(channels=6, buffer=1024)
 pygame.mixer.init()
@@ -14,25 +12,25 @@ pygame.mixer.init()
 piano_notes = {}
 skip_until = {}
 
+sensors = {0x08 : 'c', 0x09 : 'd'}
+
 for note in glob("../piano-notes/*.wav"):
     note_name = re.search("([a-z0-9]+)\.wav$", note).group(1)
     piano_notes[note_name] = pygame.mixer.Sound(note)
     skip_until[note_name] = time.time()
 
 
-note = "c"
-
 
 while True:
     #time.sleep(0.5)
+	for address in sensors:
+	    note = sensors[address]
+        distance = bus.read_byte(address)
 
-    distance = bus.read_byte(address)
-    print "Distance: " , distance 
-
-    if(distance == 0):
-        #print "No obstruction"
-        continue
-    if(time.time() >= skip_until[note]):
-        print "Playing", note
-        piano_notes[note].play()
-    skip_until[note] = time.time() + 0.5
+        if(distance == 0):
+            print "Rest ", note
+            continue
+        if(time.time() >= skip_until[note]):
+            print "Playing ", note
+            piano_notes[note].play()
+        skip_until[note] = time.time() + 0.5
